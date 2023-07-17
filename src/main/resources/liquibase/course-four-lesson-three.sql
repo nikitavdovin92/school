c588ab7afd063c3dfcfbe04b4ad86c2664c0d527
Homework3 <tradervdovin@gmail.com>
2023-07-17 16:23:27 +0500

    ДЗ миграции

--- src/main/resources/liquibase/course-four-lesson-three.sql
+++ src/main/resources/liquibase/course-four-lesson-three.sql
@@ -5,6 +5,7 @@
 --rollback DROP INDEX IDX_students_name
 CREATE INDEX IDX_students_name ON students(name);
 
+
 --changeset nikitavdovin92:2
 --precondition-sql-check expectedResult:0 select count(*) from pg_catalog.pg_tables t inner join pg_indexes i on i.tablename = t.tablename where t.tablename = 'faculties' and i.indexname = 'IDX_faculties_name_color';
 --rollback DROP INDEX IDX_faculties_name_color
